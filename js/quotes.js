8448d2ef2d13527650cae62ef9aafc9a4931d848
Daniel Usani <daniel.e.usani@gmail.com>
2017-07-31 18:50:45 -0400

    Initial app files
    - Created an array object of quotes,
    - created a getRandomQuote function

---
+++ js/quotes.js
@@ -0,0 +1,45 @@
+// created an array object with seven quotes
+var quotes = [
+  {
+    quote: "Words are easy, like the wind; Faithful friends are hard to find.",
+    source: "William Shakespeare",
+    citation: "British poet and playwright",
+    year: 1616,
+  },
+  {
+    quote: "Chose a job you love, and you will never have to work a day in your life.",
+    source: "Confucius",
+    citation: "Chinese philosopher",
+    year: "BC 479",
+  },
+  {
+    quote: "You must be the change you wish to see in the world.",
+    source: "Mahatma Gandhi",
+    citation: "Preeminent leader of Indian nationalism",
+    year: 1948,
+  },
+  {
+    quote: "The true sign of intelligence is not knowledge but imagination.",
+    source: "Albert Einstein",
+    citation: "German-Swiss-U.S. scientist",
+    year: 1955,
+  },
+  {
+    quote: "Love is the beauty of the soul.",
+    source: "Saint Augustine",
+    citation: "Theologian",
+    year: "BC 430",
+  },
+  {
+    quote: "Our greatest glory is not in never falling, but in rising every time we fall.",
+    source: "Confucius",
+    citation: "Chinese philosopher",
+    year: "BC 479",
+  },
+  {
+    quote: "Better to remain silent and be thought a fool, than to speak and remove all doubt.",
+    source: "Abraham Lincoln",
+    citation: "Politician. President of the United States",
+    year: 1865,
+  },
+]
--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+function getRandomQuote() {
+  quote = quotes[Math.floor(Math.random() * quotes.length)];
+  return quote;
+}
+
+function printQuote() {
+
+}
