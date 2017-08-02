84a52a7e02fbe114c5662418479d7ab125a12139
Daniel Usani <daniel.e.usani@gmail.com>
2017-08-02 19:30:03 -0400

    Created functions for getRandomQuote and printQuote

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
+// created a function to return a random index or number from the quotes array object.
 function getRandomQuote() {
-  quote = quotes[Math.floor(Math.random() * quotes.length)];
+  var randomArrayIndex = quotes[Math.floor(Math.random() * quotes.length)];
+  return randomArrayIndex;
+}
+
+// created a function to print a random quote from our quotes array object.
+function printQuote() {
+  quote = getRandomQuote();
+
   return quote;
 }
 
-function printQuote() {
+// console log for debugging purposes.
+console.log(printQuote());
 
-}
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote(), false);
