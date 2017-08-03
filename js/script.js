c9291a56b810bf3a78ca23f1a81b558874296259
Daniel Usani <daniel.e.usani@gmail.com>
2017-08-02 20:33:59 -0400

    Created a variable and a function
    - created an empty message variable to hold the properties of our random quote
    - created a function to help print the random quote into the div in out html.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote(), false);
+
+// created a variable to hold the contents of the div in the html. This variable
+// will be used in the printQuote function.
+var message = " ";
+
+// created a function for the div in the html to print out the contents correctly.
+function print(quote) {
+	var div = document.getElementById('quote-box');
+	div.innerHTML = quote;
+}
+
 // created a function to return a random index or number from the quotes array object.
 function getRandomQuote() {
   var randomArrayIndex = quotes[Math.floor(Math.random() * quotes.length)];
@@ -6,15 +20,11 @@ function getRandomQuote() {
 
 // created a function to print a random quote from our quotes array object.
 function printQuote() {
-  quote = getRandomQuote();
+  // gets a random quote from the array object and stores it in a variable
+  randomQuote = getRandomQuote();
 
-  return quote;
+  message = '<p class="quote">' + randomQuote.quote + '</p>';
+  message += '<p class="source">' + randomQuote.source + '<span class="citation">' + randomQuote.citation + '</span><span class="year">' + randomQuote.year + '</span></p>';
+
+  print(message);
 }
-
-// console log for debugging purposes.
-console.log(printQuote());
-
-
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote(), false);
