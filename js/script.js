6f253c675a6ee763d8de125d5a6dba1fc7aade1e
Daniel Usani <daniel.e.usani@gmail.com>
2017-08-02 20:39:34 -0400

    Fixed the onClick event for changing into random quotes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote(), false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 // created a variable to hold the contents of the div in the html. This variable
 // will be used in the printQuote function.
