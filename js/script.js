958a5a3685efe433e8c19613f5a8eed2432e52db
Daniel Usani <daniel.e.usani@gmail.com>
2017-08-04 12:47:22 -0400

    Created a variable to hold my spliced quotes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 // will be used in the printQuote function.
 var message = "";
 
+// created an empty array variable. This empty array will hold the objects
+// that are spliced or removed from my quotes array object. That way quotes
+// quotes are not duplicated until they have been displayed at least once.
+var displayedQuotes = [];
+
 // created a function for the div in the html to print out the contents correctly.
 function print(quote) {
 	var div = document.getElementById('quote-box');
@@ -15,7 +20,21 @@ function print(quote) {
 // created a function to return a random index or number from the quotes array object.
 function getRandomQuote() {
   var randomArrayIndex = quotes[Math.floor(Math.random() * quotes.length)];
-  return randomArrayIndex;
+
+	// created a variable to hold my spliced quote. This spliced qoute is then
+	// pushed or added to my empty array named displayedQuotes.
+	var spliceQuote = quotes.splice(randomArrayIndex, 1)[0];
+	displayedQuotes.push(spliceQuote);
+
+	// This is a condition that checks if my initial quotes array object is empty.
+	// If it is, then replace all the values with the displayedQuotes array. Also
+	// make the displayedQuotes array empty again.
+	if (quotes.length === 0){
+		quotes = displayedQuotes;
+		displayedQuotes = [];
+	};
+
+  return spliceQuote;
 }
 
 // created a function to print a random quote from our quotes array object.
